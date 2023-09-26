1d20a969c1073d3b2d8d3fc1e64114d4cdc5b5e5
«Kristina» <kristinka007_92@mail.ru>
2023-09-26 12:22:24 +0300

    1 commit

--- Task_1.java
+++ Task_1.java
@@ -1,2 +1,29 @@
-// Задание 4 из презентации
-// Где сама презентация? В прикрепленных файлах нет документа.
+// Реализуйте метод, который запрашивает у пользователя ввод дробного числа (типа float), и возвращает введенное значение. 
+// Ввод текста вместо числа не должно приводить к падению приложения, вместо этого, необходимо повторно запросить у пользователя ввод данных.
+
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Task_1 {
+    public static void main(String[] args) {
+        try {
+            getFloat();
+            System.out.println(getFloat());
+        } catch (InputMismatchException e) {
+            System.out.println("Ошибка! Введите дробное число, типа float");
+
+        }
+
+    }
+
+    public static Float getFloat() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите дробное число: ");
+        Float floaat = in.nextFloat();
+
+        in.close();
+
+        return floaat;
+    }
+}
