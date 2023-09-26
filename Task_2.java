9c13b1f4c9751d3cbdd9b566350cfeed813d4dec
«Kristina» <kristinka007_92@mail.ru>
2023-09-26 12:02:31 +0300

    3 commit

--- Task_2.java
+++ Task_2.java
@@ -1,3 +1,5 @@
+// Если необходимо, исправьте данный код (задание 2 https://docs.google.com/document/d/17EaA1lDxzD5YigQ5OAal60fOFKVoCbEJqooB9XfhT7w/edit)
+
 public class Task_2 {
     public static void main(String[] args) {
         try {
